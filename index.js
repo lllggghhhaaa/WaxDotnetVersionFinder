3350bcad92c07b6b227da2102fb9bffe26e223ff
lllggghhhaaa <lgha1423@outlook.com>
2024-01-10 11:39:28 -0300

    set-output deprecation fixed

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const { readFileSync } = require("fs");
+const { EOL } = require("os")
+const { readFileSync, appendFileSync } = require("fs");
 const { parseString } = require("xml2js");
 
 try {
     const path = core.getInput("path");
-
     const file = readFileSync(path);
 
     parseString(file, (err, result) => {
-        core.setOutput("version", result["Project"]["PropertyGroup"][0]["Version"][0]);
+        setOutput("version", result["Project"]["PropertyGroup"][0]["Version"][0]);
     });
 
 } catch (error) {
     console.log(error)
-    //core.setFailed(error.message);
+}
+
+function setOutput(key, value) {
+    const output = process.env['GITHUB_OUTPUT']
+    appendFileSync(output, `${key}=${value}${EOL}`)
 }
